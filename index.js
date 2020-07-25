fdbf8679a5762345c8b3158ad286e70a421f1ffa
hibeyter <hasta5634@gmail.com>
2020-07-25 17:16:11 +0300

    bug fixed

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const { apikey } = {
 };
 
 
-var wb = xlsx.readFile("adres.xlsx")
-var ws = wb.Sheets["Sayfa1"]
+var wb = xlsx.readFile("adres.xlsx") // okunacak olan xlsx dosyası 
+var ws = wb.Sheets["Sayfa1"] // okunacak olan sheet adı
+
+
 var data = xlsx.utils.sheet_to_json(ws)
 const dataLen = data.length
 var findGeo = 0;
@@ -32,17 +34,34 @@ readXlsx()
 function readXlsx() {
     data.forEach((element, index) => {
         request(opt(element.adres), (error, response, body) => {
-            if (error) throw new Error(error);
+            if (error) {
+                printXlsx(data)
+                console.log("dosya hatalı kaydedildi thrown" + findGeo)
+                throw new Error(error);
+            }
             const json = JSON.parse(body)
-            var point = json.response.GeoObjectCollection.featureMember[0]
-                .GeoObject.Point.pos
-            var space = point.indexOf(' ')
-            var pointX = point.substring(0, space)
-            var pointY = point.substring(space, point.lenght)
-            data[index].X = parseFloat(pointX)
-            data[index].Y = parseFloat(pointY)
-            findGeo++;
-            if (findGeo == dataLen) printXlsx(data)
+            try {
+                if (json.response.GeoObjectCollection.metaDataProperty.GeocoderResponseMetaData.found > 0) {
+                    var point = json.response.GeoObjectCollection.featureMember[0]
+                        .GeoObject.Point.pos
+                    var space = point.indexOf(' ')
+                    var pointX = point.substring(space, point.lenght)
+                    var pointY = point.substring(0, space)
+                    data[index].X = parseFloat(pointX)
+                    data[index].Y = parseFloat(pointY)
+                } else {
+                    data[index].X = parseFloat(0)
+                    data[index].Y = parseFloat(0)
+                }
+                findGeo++;
+                console.log("beyter " + findGeo)
+                if (findGeo == dataLen) printXlsx(data)
+            } catch (er) {
+                printXlsx(data)
+                console.log("dosya hatalı kaydedildi " + findGeo)
+                console.log(er)
+                return
+            }
         })
     })
 }
@@ -52,4 +71,5 @@ function printXlsx(data) {
     var newWS = xlsx.utils.json_to_sheet(data)
     xlsx.utils.book_append_sheet(newWB, newWS, "AllCoordinate")
     xlsx.writeFile(newWB, "NewFile.xlsx")
+    console.log("Yeni xlsx dosyası oluşturuldu ")
 }
