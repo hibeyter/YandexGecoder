35c9657aad03e6837f0edc39f1dda645a63826bc
hibeyter <hasta5634@gmail.com>
2020-08-10 18:03:39 +0300

    add address
    Kordinatlardan adres çekme eklendi

--- address.js
+++ address.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const request = require('request');
 const xlsx = require("xlsx");
 
@@ -8,14 +6,14 @@ const { apikey } = {
     apikey: 'YOUR API KEY',
 };
 
-
-var wb = xlsx.readFile("adres.xlsx") // okunacak olan xlsx dosyası 
+var wb = xlsx.readFile("kordinat.xlsx") // okunacak olan xlsx dosyası 
 var ws = wb.Sheets["Sayfa1"] // okunacak olan sheet adı
 
 
 var data = xlsx.utils.sheet_to_json(ws)
 const dataLen = data.length
 var findGeo = 0;
+
 var opt = function(adres) {
     return {
         method: 'GET',
@@ -33,7 +31,7 @@ readXlsx()
 
 function readXlsx() {
     data.forEach((element, index) => {
-        request(opt(element.adres), (error, response, body) => {
+        request(opt(element.Y + "," + element.X), (error, response, body) => {
             if (error) {
                 printXlsx(data)
                 console.log("dosya hatalı kaydedildi thrown" + findGeo)
@@ -42,16 +40,12 @@ function readXlsx() {
             const json = JSON.parse(body)
             try {
                 if (json.response.GeoObjectCollection.metaDataProperty.GeocoderResponseMetaData.found > 0) {
-                    var point = json.response.GeoObjectCollection.featureMember[0]
-                        .GeoObject.Point.pos
-                    var space = point.indexOf(' ')
-                    var pointX = point.substring(space, point.lenght)
-                    var pointY = point.substring(0, space)
-                    data[index].X = parseFloat(pointX)
-                    data[index].Y = parseFloat(pointY)
+                    var adress = json.response.GeoObjectCollection.featureMember[0]
+                        .GeoObject.metaDataProperty.GeocoderMetaData.text
+                    data[index].Adres = adress
+
                 } else {
-                    data[index].X = parseFloat(0)
-                    data[index].Y = parseFloat(0)
+                    data[index].Adres = ""
                 }
                 findGeo++;
                 console.log("Tamamlandı " + findGeo)
@@ -70,6 +64,6 @@ function printXlsx(data) {
     var newWB = xlsx.utils.book_new()
     var newWS = xlsx.utils.json_to_sheet(data)
     xlsx.utils.book_append_sheet(newWB, newWS, "Sayfa1")
-    xlsx.writeFile(newWB, "NewFile.xlsx")
+    xlsx.writeFile(newWB, "Adresler.xlsx")
     console.log("Yeni xlsx dosyası oluşturuldu ")
 }
